a8f82c2122fc204f39cb2e89b05ed6b1fb94291d
Massimiliano Mazzarolo <massimiliano.mazzarolo@gmail.com>
2013-01-28 22:43:40 +0100

    Akka & ScalaTest dependencies fixed.

--- project/AsyncCacheBuild.scala
+++ project/AsyncCacheBuild.scala
@@ -1,7 +1,7 @@
 import sbt._
 import sbt.Keys._
 
-object AsyncCacheBuild extends Build {
+object AsyncCacheBuild extends Build with Dependencies{
 
   lazy val asynccache = Project(
     id = "asynccache",
@@ -12,7 +12,32 @@ object AsyncCacheBuild extends Build {
       version := "0.1-SNAPSHOT",
       scalaVersion := "2.10.0",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-      libraryDependencies += "com.typesafe.akka" % "akka-actor_2.10" % "2.1.0"
+      libraryDependencies ++= akka ++ scalatest
+
     )
   )
+}
+
+trait Versions {
+
+  val SCALA_MAJOR = "2.10"
+  val SCALA_MINOR = "0"
+
+  val SCALA = SCALA_MAJOR + "." + SCALA_MINOR
+
+  val AKKA = "2.1.0"
+
+  val SCALA_TEST = "1.9.1"
+}
+
+trait Dependencies extends Build with Versions {
+
+  def cross(artifact: String) = artifact + "_" + SCALA_MAJOR
+
+  def module(name: String, scope: String = "compile"): ModuleID = "com.typesafe.akka" % cross(name) % AKKA % scope
+
+  val akka = Seq(module("akka-actor"), module("akka-testkit", "test"))
+
+  val scalatest =  Seq("org.scalatest" % cross("scalatest") % SCALA_TEST % "test")
+
 }
